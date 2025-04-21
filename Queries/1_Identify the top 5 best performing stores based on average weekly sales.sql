145398f45512b5a07c0ca33713bcfa65fc9d87ac
Jermaine Herman Sangiwa <88526685+jermainesangiwa@users.noreply.github.com>
2025-04-21 12:10:24 +0530

    Update and rename Query 1.sql to 1_Identify the top 5 best performing stores based on average weekly sales.sql

--- Queries/1_Identify the top 5 best performing stores based on average weekly sales.sql
+++ Queries/1_Identify the top 5 best performing stores based on average weekly sales.sql
@@ -1,4 +1,4 @@
--- Identify the top 5 best performing stoes based on average weekly sales.
+-- Identify the top 5 best performing stores based on average weekly sales.
 SELECT
     store_no,
     ROUND(AVG(Weekly_Sales), 2) AS Average_Sales
