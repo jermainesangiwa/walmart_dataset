be8aa0cc2184ab36eba1fcd868e7c87845fa1bde
Jermaine Herman Sangiwa <88526685+jermainesangiwa@users.noreply.github.com>
2025-04-21 12:16:54 +0530

    Delete Identify the top 5 best performing stores based on average weekly sales.sql

--- Identify the top 5 best performing stores based on average weekly sales.sql
+++
@@ -1,8 +0,0 @@
--- Identify the top 5 best performing stores based on average weekly sales.
-SELECT
-    store_no,
-    ROUND(AVG(Weekly_Sales), 2) AS Average_Sales
-FROM walmart_sales
-GROUP BY store_no
-ORDER BY Average_Sales DESC
-LIMIT 5;
